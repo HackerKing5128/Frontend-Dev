80560bb17e520be17f7ea01159f67a176c326769
Nakul Saini <kanhasaini2017@gmail.com>
2025-11-11 22:35:48 +0530

    Submission of Assignment 1

---
+++ intro/conditions.js
@@ -0,0 +1,13 @@
+let grade = Math.random() * 10;
+
+if (grade >= 9) {
+  console.log("A");
+} else if (grade >= 8) {
+  console.log("B");
+} else if (grade >= 7) {
+  console.log("C");
+} else if (grade >= 6) {
+  console.log("D");
+} else {
+  console.log("F");
+}
---
+++ intro/diagonalSquare.js
@@ -0,0 +1,5 @@
+let side = 10;
+
+// diagonal length calculation
+let diagonalLength = Math.sqrt(2 * Math.pow(side, 2));
+console.log("Diagonal Length:", diagonalLength);
